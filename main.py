b9fb092aa77930fa5fac8febe26d26cca67c9e0d
Hanine <hanineramdhane@gmail.com>
2025-11-18 09:19:12 +0100

    2eme ajout de contenu main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from netmiko import ConnectHandler
+def dire_salut():
+    print("Salut, Git!")
 
-def acces_netmiko():
-    cisco_device = {
-        "device_type": "cisco_xr",
-        "host": "sandbox-iosxr-1.cisco.com",
-        "username": "admin",
-        "password": "C1sco12345",
-        "port": 22,
-    }
-
-    net_connect = ConnectHandler(**cisco_device)
-    clock = net_connect.send_command("show clock")
-    print("Heure du routeur :", clock)
-
-    interfaces = net_connect.send_command("show ip interface brief")
-    with open("interfaces.txt", "w") as f:
-        f.write(interfaces)
-
-acces_netmiko()
+dire_salut()
